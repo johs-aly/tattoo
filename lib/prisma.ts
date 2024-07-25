34372f991557583f07c0d0a3031165e5dd9d3f4d
srd17332397262 <shaokangnetwork@foxmail.com>
2024-07-25 11:51:38 +0800

    test

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,18 @@
-import { PrismaClient } from "@prisma/client";
-
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient
-}
-
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
-export default prisma;
+// import { PrismaClient } from "@prisma/client";
+//
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var prisma: null
+// }
+//
+// // let prisma: PrismaClient;
+// //
+// // if (process.env.NODE_ENV === "production") {
+// //   prisma = new PrismaClient();
+// // } else {
+// //   if (!global.prisma) {
+// //     global.prisma = new PrismaClient();
+// //   }
+// //   prisma = global.prisma;
+// // }
+// export default prisma;
